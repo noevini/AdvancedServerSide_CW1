05dbb1d17253bfd08cbd07a106e28d21a6d12777
vinicius <noefvini@gmail.com>
2026-03-11 14:35:24 +0000

    feat: add base middleware stack

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const routes = require("./routes");
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/", routes);
 

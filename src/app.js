39bc5fe3226ecbb166700d451212a772d8b812bf
vinicius <noefvini@gmail.com>
2026-03-11 14:27:37 +0000

    refactor: move root route to routes layer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+app.use("/", routes);
 
 module.exports = app;

d2f046a2f02eeffe12669b1720e68e923da74eb4
vinicius <noefvini@gmail.com>
2026-03-30 17:52:03 +0100

    refactor name change

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 
 const userRepository = require("../repositories/userRepository");
 const emailVerificationTokenRepository = require("../repositories/emailVerificationTokenRepository");
-const passwordResetTokenRepository = require("../repositories/passwordResetTokenRepository");
+const passwordResetRepository = require("../repositories/passwordResetRepository");
 
 const authService = {
   registerUser: async (email, password) => {
